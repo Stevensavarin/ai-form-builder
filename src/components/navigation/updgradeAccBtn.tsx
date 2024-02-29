6b7eb4c9eb14817057e9e33b079c09a149d42a89
steven_savarin <srssdesing@gmail.com>
2024-02-29 11:25:40 +0100

    last config

--- src/components/navigation/updgradeAccBtn.tsx
+++ src/components/navigation/updgradeAccBtn.tsx
@@ -28,7 +28,7 @@ const UpdgradeAccBtn = async (props: Props) => {
       <ProgressBar value={percent} />
       <p className='mt-2'>{formCount} out of {MAX_FREE_FROMS} forms generated.</p>
       <p>
-        <SubscribeBtn price="price_1Oeu01C0XQCoR9vaO7GKAKRJ" userId={userId} />
+        <SubscribeBtn price="price_1Op6JEKsWV3lHfMkfsZ1JEmV" userId={userId} />
         {' '} for unlimited forms.
       </p>
     </div>
