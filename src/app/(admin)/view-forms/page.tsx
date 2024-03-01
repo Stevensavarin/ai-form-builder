cda31366b4713c887414cfb261e20f51709e5a40
steven_savarin <srssdesing@gmail.com>
2024-03-01 13:13:48 +0100

    home config

--- src/app/(admin)/view-forms/page.tsx
+++ src/app/(admin)/view-forms/page.tsx
@@ -4,34 +4,15 @@ import { getUserForms } from '@/app/actions/getUserForms'
 import { InferSelectModel } from 'drizzle-orm'
 import { forms as dbForms } from "@/db/schema";
 
-type Props = {
-  forms: InferSelectModel<typeof dbForms>[];
-}
+type Props = {}
 
-const Page = ({ forms }: Props) => {
+const page = async (props: Props) => {
+  const forms: InferSelectModel<typeof dbForms>[] = await getUserForms();
+
   return (
     <>
-      <FormsList forms={forms} />
-    </>
+      <FormsList forms={forms} /></>
   )
 }
 
-export default Page;
-
-export async function getServerSideProps() {
-  try {
-    const forms: InferSelectModel<typeof dbForms>[] = await getUserForms();
-    return {
-      props: {
-        forms
-      }
-    }
-  } catch (error) {
-    console.error("Error fetching user forms:", error);
-    return {
-      props: {
-        forms: []
-      }
-    }
-  }
-}
+export default page
